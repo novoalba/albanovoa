a190c67b1997002e6695c6fcd5c87c09b142c1fc
4lb-ita <honnneygi@gmail.com>
2023-09-24 20:19:08 -0600

    Primer commit: Agregar sitio de WordPress

---
+++ wp-content/themes/zino/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4861af3edd33ef03556ede49075b70bcfc6c7d79',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4861af3edd33ef03556ede49075b70bcfc6c7d79',
+    ),
+    'ernilambar/wp-admin-notice' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '626e4685775bdd06085e7af76f9ae9380c68c9f0',
+    ),
+  ),
+);
